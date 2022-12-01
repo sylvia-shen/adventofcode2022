2acc62698ba96ba8ffe6c9f8623834569a313021
Sylvia Shen <s.shen@catawiki.nl>
2022-12-01 11:18:30 +0100

    day 1 part 2

--- day1/run.rb
+++ day1/run.rb
@@ -2,6 +2,8 @@ file_data = File.read("input")
 
 grouped = file_data.split("\n\n")
 grouped_sum = grouped.map { |t| t.split("\n").map(&:to_i).sum }
-answer = grouped_sum.sort.last
+sorted = grouped_sum.sort
 
-puts answer
+puts "Part1: #{sorted.last}"
+puts "Part2: #{sorted.last(3).sum}"
+
